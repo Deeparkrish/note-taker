3ad41e99ec14f1ca335a8374b69bc5a158ecbdc0
Deepa <kosalai17@gmail.com>
2021-07-14 17:24:09 -0700

    able to add data POST works

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -3,6 +3,17 @@ const fs= require('fs')
 const router = require('express').Router();
 var data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
 
+function validateNote(note){
+
+  if (!note.title || typeof note.title !== 'string') {
+      return false;
+  }
+  if (!note.text || typeof note.text !== 'string') {
+      return false;
+  }
+  return true;
+}
+
 //  return all  saved notes
 router.get('/notes',(req,res)=>{
 
@@ -15,7 +26,6 @@ router.get('/notes',(req,res)=>{
 
 //function  to set up a route on our server that accepts data to be stored server-side.
 router.post('/notes',(req,res)=>{
-
     // req.body is where our incoming content will be
     // set id based on what the next index of the array will be
     req.body.id = data.length.toString(); //assign a unique id 
@@ -27,33 +37,30 @@ router.post('/notes',(req,res)=>{
     res.status(400).send("The notes is not correct.");
   } else {  
     // write it in  to the  file 
-    fs.writeFileSync(
-      path.join(__dirname, "../db/db.json"),
+    fs.writeFileSync("./db/db.json",
       // converts data into json string .
-      JSON.stringify(data),function (err){if (err)throw(err)});
-    res.json(data);  //json response the note added.
+      JSON.stringify(data),function (err){if (err)throw(err)}
+      );
+      res.json(data);  //json response the note added.
   }
 
 });
 
-// router.get("/notes/:id", (req, res) => {
-//   const result = findNoteById(req.params.id, data);
-//   if (result) {
-//     res.json(result);
-//   } else {
-//     res.send(404);
-//   }
-// });
+router.get("/notes/:id", (req, res) => {
+  // const result = findNoteById(req.params.id, data);
+  let noteId = Number(req.params.id);
 
-function validateNote(note){
+  if (noteId < data.length) {
+    res.json(data[noteId]);
+  } else {
+    res.send(404);
+  }
+});
 
-  if (!note.title || typeof note.title !== 'string') {
-      return false;
-  }
-  if (!note.text || typeof note.text !== 'string') {
-      return false;
-  }
-  return true;
-}
+// Delete  by id , find the id in the file and remove the corresponding data. 
+// filter the rest .
+//  // const result = data.filter((note) => note.id !== noteId)[0];
+
+// write to file again 
 
 module.exports =router;
